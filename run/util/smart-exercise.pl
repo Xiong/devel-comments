1664e3347762edbe22b134720188c397e3859c39
Xiong Changnian <xiong@xuefang.com>
2010-05-30 15:01:49 -0700

    fiddle with the demo

--- run/util/smart-exercise.pl
+++ run/util/smart-exercise.pl
@@ -12,23 +12,43 @@ use lib qw{
 	../../run/t
 };
 
-use Smart::Comments;	# Enable special comments for debugging and reporting
+use Smart::Comments;	# Enable special comments for debugging
 #use Smart::Comments::Any;	
 
-my $data_structure = {
+my $data_structure 	= {
     a => [ 1, 2, 3 ],
 };
+my $scalar			= 42;
+
+my %very			;
+my $index			= 18;
+$very{long}{thing}[$index] = 'main';
+### $very{long}{thing}[18]
+
+#	no strict 'refs';		# disable complaint about symbolic reference
+#	no warnings 'once';		# disable complaint about var only used once
+#	${ *{"${caller_ns}\::smart-comments-outfh"} }	= 'toejam';
+#	### ${ *{"${caller_ns}\::smart-comments-outfh"} }
+	
+#	use warnings;
+#	use strict;
+
 
 ### This is a comment.
-### <now>
+### <now> I'm happy.
 ### $data_structure
+### $scalar
 
-my $max		= 2**3;
-my $idle	;
+#my $max		= 2**4;
+my $max		= 2**2;
 
-for (0..$max) {			### for...			done
-	sleep(1);
-};
+#for (0..$max) {			### for1...			done
+#	sleep(1);
+#};
+
+#for (0..$max) {			### for2 |===[%]    |
+#	sleep(1);
+#};
 
 __END__
 
